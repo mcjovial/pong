8615846dd83b62ae1fecaa6e109308ae98fbcd09
mcjovial <emmanuelelias455@gmail.com>
2021-11-29 15:27:06 +0100

    double player functionality and paddle sync

--- javascripts/script.js
+++ javascripts/script.js
@@ -2,6 +2,7 @@
 const canvas = document.createElement('canvas');
 const context = canvas.getContext('2d');
 const socket = io('http://localhost:3000');
+let isReferee = false;
 let paddleIndex = 0;
 
 let width = 500;
@@ -177,11 +178,14 @@ function animate() {
 }
 
 // Start Game, Reset Everything
-function startGame() {
+function loadGame() {
   createCanvas();
   renderIntro();
-  
-  paddleIndex = 0;
+  socket.emit('ready');
+}
+
+function startGame() {
+  paddleIndex = isReferee ? 0 : 1;
   window.requestAnimationFrame(animate);
   canvas.addEventListener('mousemove', (e) => {
     playerMoved = true;
@@ -192,11 +196,30 @@ function startGame() {
     if (paddleX[paddleIndex] > (width - paddleWidth)) {
       paddleX[paddleIndex] = width - paddleWidth;
     }
+    socket.emit('paddleMove', {
+      xPosition: paddleX[paddleIndex],
+    })
     // Hide Cursor
     canvas.style.cursor = 'none';
   });
 }
 
 // On Load
-startGame();
+loadGame();
 
+socket.on('connect', () => {
+  console.log('Connected as ...', socket.id);
+});
+
+socket.on('startGame', (refereeId) => {
+  console.log('Referee is ', refereeId);
+
+  isReferee = socket.io === refereeId;
+  startGame();
+});
+
+socket.on('paddleMove', (paddleData) => {
+  // Toggle 1 into 0 and 0 into 1
+  const opponentPaddleIndex = 1 - paddleIndex;
+  paddleX[opponentPaddleIndex] = paddleData.xPosition;
+})
--- server.js
+++ server.js
@@ -7,5 +7,19 @@ server.listen(PORT);
 console.log(`Listening on port ${PORT}...`);
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    console.log('a user connected', socket.id);
+
+    socket.on('ready', () => {
+        console.log('Player ready', socket.io);
+
+        readyPlayerCount++;
+
+        if (readyPlayerCount === 2) {
+            io.emit('startGame', socket.id);
+        }
+    });
+
+    socket.on('paddleMove', (paddleData) => {
+        socket.broadcast.emit('paddleMove', paddleData);
+    });
 })
