19e311594c4d3bb6021f5ef1e1920603c1bed991
mcjovial <emmanuelelias455@gmail.com>
2021-11-29 16:27:23 +0100

    setting up express server for front-end and integrating with http & socket.io servers

--- server.js
+++ server.js
@@ -1,33 +1,14 @@
-const server = require('http').createServer();
-const io = require('socket.io')(server);
+const http = require('http');
+const io = require('socket.io');
 
+const api = require('./api');
+const httpServer = http.createServer(api);
+const socketServer = io(httpServer);
+
+const sockets = require('./sockets');
 const PORT = 3000;
 
 server.listen(PORT);
 console.log(`Listening on port ${PORT}...`);
 
-io.on('connection', (socket) => {
-    console.log('a user connected', socket.id);
-
-    socket.on('ready', () => {
-        console.log('Player ready', socket.io);
-
-        readyPlayerCount++;
-
-        if (readyPlayerCount % 2 === 0) {
-            io.emit('startGame', socket.id);
-        }
-    });
-
-    socket.on('paddleMove', (paddleData) => {
-        socket.broadcast.emit('paddleMove', paddleData);
-    });
-
-    socket.on('ballMove', (ballData) => {
-        socket.broadcast.emit('ballMove', ballData);
-    });
-
-    socket.on('disconnect', (reason) => {
-        console.log(`CLient ${socket.id} disconnected: ${reason}`);
-    });
-})
+sockets.listen(socketServer);
